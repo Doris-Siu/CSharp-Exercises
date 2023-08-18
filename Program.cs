cefc90f9897a61dafc31941f8a9bd5bcdb2ea9db
Doris <107772913+Doris-Siu@users.noreply.github.com>
2023-08-18 17:30:16 +0100

    complete exercise and review codes

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using CSharp_stopWatchExercise;
+
+var stopwatch = new Stopwatch();
+
+while (true)
+{
+    stopwatch.Start();
+    Thread.Sleep(2000);
+    stopwatch.Stop();
+
+
+    Console.WriteLine("The duration between start and stop is " + stopwatch.GetDuration());
+    Console.Write("Press Enter to continue: ");
+    Console.ReadLine();
+
+
+}
---
+++ Stopwatch.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace CSharp_stopWatchExercise
+{
+	public class Stopwatch
+	{
+		private DateTime _start;
+		private DateTime _end;
+		private bool _running;
+
+		public void Start()
+		{
+			if (_running)
+			{
+				throw new InvalidOperationException("The stopwatch is running already..");
+			};
+
+			_start = DateTime.Now;
+			_running = true;
+
+
+		}
+
+		public void Stop()
+		{
+            if (!_running)
+            {
+                throw new InvalidOperationException("The stopwatch is not running..");
+            };
+
+			_end = DateTime.Now;
+			_running = false;
+        }
+
+		public TimeSpan GetDuration()
+		{
+			return _end - _start;
+		}
+
+    }
+}
+
